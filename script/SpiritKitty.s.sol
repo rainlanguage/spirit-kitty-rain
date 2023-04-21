879c111daf9d49bac5aba1ae4291ecdf1416e829
marcusnewton <marcus.newton@hey.com>
2023-04-21 06:10:19 +0000

    script setup

--- script/SpiritKitty.s.sol
+++ script/SpiritKitty.s.sol
@@ -1,43 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "lib/forge-std/src/Script.sol";
-import "lib/rain.interface.factory/src/ICloneFactoryV1.sol";
-import {FlowERC721Config, IFlowERC721V2, EvaluableConfig} from "lib/rain.interface.flow/src/IFlowERC721V2.sol";
+import "../lib/forge-std/src/Script.sol";
+import "../lib/forge-std/src/StdJson.sol";
+import "../lib/rain.interface.factory/src/ICloneFactoryV1.sol";
+import {FlowERC721Config, IFlowERC721V2, EvaluableConfig, IExpressionDeployerV1} from "../lib/rain.interface.flow/src/IFlowERC721V2.sol";
 
 contract SpiritKitty is Script {
-    IFlowERC721V2 flow_;
+    using stdJson for address;
+    using stdJson for string;
 
-    address cloneFactoryAddress = 0x7e58B418bAA10e812884D4ABbE5e72593EFFE471;
-    address flowERC721Address = 0xBAc970976b1e3293c640e045acA4ee83f0231A5C;
-    address expressionDeployerAddress =
-        0x703A4C3Fae78A3F57894c1093Ae1D06750939D0F;
+    string json;
+    IFlowERC721V2 flowERC721;
 
     function setUp() public {
-        // struct FlowERC721Config {
-        //     string name;
-        //     string symbol;
-        //     string baseURI;
-        //     EvaluableConfig evaluableConfig;
-        //     EvaluableConfig[] flowConfig;
-        // };
-        // struct EvaluableConfig {
-        //     IExpressionDeployerV1 deployer;
-        //     bytes[] sources;
-        //     uint256[] constants;
-        // }
+        // Read from the config file
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/script/spiritKitty.json");
+        json = vm.readFile(path);
+
         ICloneableFactoryV1 cloneFactory_ = ICloneableFactoryV1(
-            cloneFactoryAddress
+            stdJson.readAddress(json, ".cloneFactory")
         );
-        flow_ = cloneFactory_.clone(
-            flowERC721Address,
-            FlowERC721Config(
-                "SpiritKitty",
-                "SK",
-                "https://arpet.club/kitty-corner/",
-                EvaluableConfig(expressionDeployerAddress, [], []),
-                []
-            )
+        IFlowERC721V2 flowERC721_ = IFlowERC721V2(
+            stdJson.readAddress(json, ".flowERC721")
+        );
+        IExpressionDeployerV1 expressionDeployer_ = IExpressionDeployerV1(
+            stdJson.readAddress(json, ".expressionDeployer")
+        );
+
+        uint256[] memory constants = stdJson.readUintArray(json, ".constants");
+        bytes[] memory sources = stdJson.readBytesArray(json, ".sources");
+        EvaluableConfig[] memory flows;
+
+        FlowERC721Config memory config = FlowERC721Config(
+            stdJson.readString(json, ".name"),
+            stdJson.readString(json, ".symbol"),
+            stdJson.readString(json, ".baseURI"),
+            EvaluableConfig(expressionDeployer_, sources, constants),
+            flows
+        );
+
+        flowERC721 = IFlowERC721V2(
+            cloneFactory_.clone(address(flowERC721_), abi.encode(config))
         );
     }
 
